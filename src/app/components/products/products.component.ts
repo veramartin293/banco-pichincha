0f3c07c0b587c627a656bed86709c745850634fd
Martin Vera <veramartin293@gmail.com>
2023-10-26 18:26:44 -0600

    Funcionalidad de obtención de productos desde API

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import { Product } from 'src/app/models/product.model';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent {
+  products$: Observable<Product[]> = this.productService.products$.pipe(
+    tap((products) => {
+      this.numResults = products.length;
+    })
+  );
+  numResults = 0;
 
+  constructor(private productService: ProductService) {}
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -2,18 +2,42 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Product } from '../models/product.model';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
+  products$ = this.getAllProducts();
+
   constructor(private http: HttpClient) {}
 
-  getAllProducts() {
-    return this.http.get(`${environment.API_URL}/bp/products`, {
-      headers: { authorId: '223' },
-    });
+  private getAllProducts(): Observable<Product[]> {
+    return this.http
+      .get<Product[]>(`${environment.API_URL}/bp/products`, {
+        headers: { authorId: '223' },
+      })
+      .pipe(
+        tap((products) => {
+          products.forEach((product) => {
+            product.date_release = this.formatDateString(product.date_release);
+            product.date_revision = this.formatDateString(
+              product.date_revision
+            );
+          });
+        })
+      );
+  }
+
+  // Date expected as YYYY-MM-DDTHH:mm:ss+00:00
+  private formatDateString(date: string): string {
+    const datePart = date.split('T')[0];
+    const dateParts = datePart.split('-');
+    const year = dateParts[0];
+    const month = dateParts[1];
+    const day = dateParts[2];
+    let formatedDate: string = `${day}/${month}/${year}`;
+    return formatedDate;
   }
 
   saveProduct(product: Product): Observable<any> {
